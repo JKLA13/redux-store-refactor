5c3cb97f6374ca348edeb75505aef437c6537da6
Jacob Adamson <jakeladamson0@gmail.com>
2022-05-16 21:21:05 -0600

    updated reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -20,7 +20,7 @@ const initialState = {
 };
 
 // TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
-export const reducer = (state, action) => {
+export const reducers = (state = initialState, action) => {
   switch (action.type) {
     // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
     // Your comment here
@@ -101,6 +101,4 @@ export const reducer = (state, action) => {
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
+export default reducers;
